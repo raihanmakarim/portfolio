b99c5eebcf24430465721929f8d2127a747f3465
raihanmakarim <raihanmakarim21@gmail.com>
2022-08-30 12:52:15 +0700

    add changes to globalstyle,index,js, and app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => (
+    <Router>
+
+    </Router>
+);
+
+
+
+
+
+export default App;
